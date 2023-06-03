a7709c976eb0e4912ed7afab4291070efe8b7b78
Ahme_if16 <Ahme_if16@STHQ0113-04>
2023-06-03 17:37:27 +0400

    Add project files.

---
+++ SocialNetworkWithSignalR/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SocialNetworkWithSignalR.Entities;
+using SocialNetworkWithSignalR.Models;
+using System.Diagnostics;
+
+namespace SocialNetworkWithSignalR.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class HomeController : Controller
+    {
+        private UserManager<CustomIdentityUser> _userManager { get; set; }
+        private CustomIdentityDbContext _context;
+
+        public HomeController(UserManager<CustomIdentityUser> userManager, CustomIdentityDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            ViewBag.User = user;
+            return View();
+        }
+    }
+}
---
+++ SocialNetworkWithSignalR/Entities/Friend.cs
@@ -0,0 +1,11 @@
+﻿namespace SocialNetworkWithSignalR.Entities
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+        public string OwnId { get; set; } = null!;
+        public string YourFriendId { get; set; } = null!;
+        public virtual CustomIdentityUser? YourFriend { get; set; } 
+
+    }
+}
